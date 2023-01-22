4e86dc5b821f4a000b56c98cd8698f734bb55678
Etai Tavor <mntetai@gmail.com>
2023-01-22 09:18:22 +0000

    added aws region as a variable

--- Homework1/variables.tf
+++ Homework1/variables.tf
@@ -3,3 +3,9 @@ variable "instance_count" {
   description = "Number of instances"
   default     = 2
 }
+
+variable "AWS_region" {
+  type        =  string
+  description = "Aws region"
+  default     = "us-east-1"
+}

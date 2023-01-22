d23a297beb8dfe23b8190c34e3039aef189b4b05
Etai Tavor <mntetai@gmail.com>
2023-01-22 15:25:56 +0000

    Added html to home page

--- Homework1/provision.sh
+++ Homework1/provision.sh
@@ -2,4 +2,4 @@
   sudo amazon-linux-extras install -y nginx1
   sudo service nginx start
   sudo rm /usr/share/nginx/html/index.html
-  echo "Welcome to Grandpa's Whiskey" | sudo tee /usr/share/nginx/html/index.html
+  echo "<html><head><title>Grandpa's Whiskey</title></head><body>Welcome to Grandpa's Whiskey</body></html>" | sudo tee /usr/share/nginx/html/index.html

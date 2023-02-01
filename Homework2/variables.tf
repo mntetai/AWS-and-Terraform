8fa50571e32a56bdad0ee9177b94dcbe6121467e
Etai Tavor <mntetai@gmail.com>
2023-02-01 08:47:02 +0000

    moved nginx to private subnet , added capacity for instances

--- Homework2/variables.tf
+++ Homework2/variables.tf
@@ -16,26 +16,51 @@ variable "map_public_ip_on_launch" {
   default     = true
 }
 
-variable "db_instances_count" {
+variable "private_subnet_count" {
   type        = number
-  description = "number of db instances"
+  description = "number of private subnets"
   default     = 2
+}
+
+variable "public_subnet_count" {
+  type        = number
+  description = "number of public subnets"
+  default     = 2
+}
+
+variable "public_cidrs" {
+  type        = list(string)
+  description = "public subnets cidr blocks"
+  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "private_cidrs" {
+  type        = list(string)
+  description = "private subnet cidr blocks"
+  default     = ["10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "nginx_instances_count" {
   type        = number
   description = "number of nginx instances"
-  default     = 2
+  default     = 5
 }
 
-variable "nginx_cidrs" {
-  type        = list(string)
-  description = "nginx cidr blocks"
-  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+variable "db_instances_count" {
+  type        = number
+  description = "number of db instances"
+  default     = 5
 }
 
-variable "db_cidrs" {
-  type        = list(string)
-  description = "nginx cidr blocks"
-  default     = ["10.0.2.0/24", "10.0.3.0/24"]
+# cidr range addreses #
+variable "vpc_cidr_range" {
+  type        = string
+  description = "vpc cider range"
+  default     = "10.0.0.0/16"
+}
+
+variable "internet_cidr_range" {
+  type        = string
+  description = "internet cidr range"
+  default     = "0.0.0.0/0"
 }

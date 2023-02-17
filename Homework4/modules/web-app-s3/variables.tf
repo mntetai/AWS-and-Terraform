ea7185cc6e7f657dea40b4d5cdf8909ac72ce4a9
Etai Tavor <mntetai@gmail.com>
2023-02-17 12:06:03 +0000

    deleted local modules

--- Homework4/modules/web-app-s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  type        = string
-  description = "Name of the S3 bucket to create"
-}
-
-variable "elb_service_account_arn" {
-  type        = string
-  description = "ARN of ELB service account"
-}
